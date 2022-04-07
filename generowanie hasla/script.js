2e6995f56b3a51d179acb346c0006c85c2e1810b
ZlomekSynek <97887069+ZlomekSynek@users.noreply.github.com>
2022-04-07 15:49:39 +0200

    Add files via upload

---
+++ generowanie hasla/script.js
@@ -0,0 +1,16 @@
+var iloscZn=8
+var iloscCyfr=0
+var iloscMalychL=0
+function iloscZnakow(){
+    iloscZn=document.getElementById("iloscZnakow").value
+    document.getElementById("iloscCyfr").max=iloscZn
+    document.getElementById("iloscMaleL").max=iloscZn
+}
+function wybCyfr(){
+    iloscCyfr=document.getElementById("iloscCyfr").value
+    document.getElementById("wybCyfr").value=iloscCyfr
+}
+function wybMaleL(){
+    iloscCyfr=document.getElementById("iloscMaleL").value
+    document.getElementById("wybMaleL").value=iloscCyfr
+}
