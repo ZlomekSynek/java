ddd62b408e2e9cf74dac6c3fe651deccbc71daea
ZlomekSynek <97887069+ZlomekSynek@users.noreply.github.com>
2022-04-11 15:49:59 +0200

    Add files via upload

--- generowanie hasla/script.js
+++ generowanie hasla/script.js
@@ -1,16 +1,39 @@
 var iloscZn=8
 var iloscCyfr=0
-var iloscMalychL=0
+var iloscMaleL=0
+var iloscWolnychZn=0
+var iloscDuzeL=0
+var iloscZnSpec=0
+
 function iloscZnakow(){
     iloscZn=document.getElementById("iloscZnakow").value
-    document.getElementById("iloscCyfr").max=iloscZn
-    document.getElementById("iloscMaleL").max=iloscZn
+    ustawWolne()
 }
 function wybCyfr(){
     iloscCyfr=document.getElementById("iloscCyfr").value
     document.getElementById("wybCyfr").value=iloscCyfr
+    ustawWolne()
 }
 function wybMaleL(){
-    iloscCyfr=document.getElementById("iloscMaleL").value
-    document.getElementById("wybMaleL").value=iloscCyfr
+    iloscMaleL=document.getElementById("iloscMaleL").value
+    document.getElementById("wybMaleL").value=iloscMaleL
+    ustawWolne()
+}
+function wybDuzeL(){
+    iloscDuzeL=document.getElementById("iloscDuzeL").value
+    document.getElementById("wybDuzeL").value=iloscDuzeL
+    ustawWolne()
+}
+function wybZnSpec(){
+    iloscZnSpec=document.getElementById("iloscZnSpec").value
+    document.getElementById("wybZnSpec").value=iloscZnSpec
+    ustawWolne()
+}
+function ustawWolne(){
+    iloscWolnychZn=iloscZn-iloscCyfr-iloscMaleL-iloscDuzeL-iloscZnSpec
+    document.getElementById("wynik").value=iloscWolnychZn
+    document.getElementById("iloscCyfr").max=+iloscCyfr+ +iloscWolnychZn
+    document.getElementById("iloscMaleL").max=+iloscMaleL+ +iloscWolnychZn
+    document.getElementById("iloscDuzeL").max=+iloscDuzeL+ +iloscWolnychZn
+    document.getElementById("iloscZnSpec").max=+iloscZnSpec+ +iloscWolnychZn
 }
