3d89e659e11990041641e1c7c970e9278d627af6
ZlomekSynek <97887069+ZlomekSynek@users.noreply.github.com>
2022-04-22 08:44:53 +0200

    Add files via upload

--- generowanie hasla/script.js
+++ generowanie hasla/script.js
@@ -40,10 +40,33 @@ function ustawWolne(){
 function generujHaslo(){
     let haslo='';
     const alfabetMale="abcdefghijklmnoprstuvwxyz";
+    const alfabetDuze="ABCDEFGHIJKLMNOPRSTUVWXYZ"
+    const alfabetCyfry="0123456789"
+    const alfabetZnSpec="!@#$%^&*"
 
     //losowanie malych liter
     for(let i=0;i<iloscMaleL;i++){
         haslo+=alfabetMale.charAt(Math.floor(Math.random()*alfabetMale.length));
     }
+    for(let i=0;i<iloscDuzeL;i++){
+        haslo+=alfabetDuze[Math.floor(Math.random()*alfabetDuze.length)];
+    }
+    for(let i=0;i<iloscCyfr;i++){
+        haslo+=Math.floor(Math.random()*10);
+    }
+    for(let i=0;i<iloscZnSpec;i++){
+        haslo+=alfabetZnSpec.charAt(Math.floor(Math.random()*alfabetZnSpec.length));
+    }
     document.getElementById("test").value=haslo;
+}
+function zmianaKolejnosci(haslo){
+    let wynik=haslo.split('');
+    for(let i=0;i<wynik.length;i++){
+        let los=Math.floor(Math.random()*wynik.length);
+        let tmp=wynik[i];
+        wynik[i]=wynik[los];
+        wynik[los]=tmp;
+    }
+    haslo=wynik.join('');
+    return haslo;
 }
