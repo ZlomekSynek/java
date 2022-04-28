fbbe839b7d3c8d09c810c46bc38c7ebe762a2145
ZlomekSynek <97887069+ZlomekSynek@users.noreply.github.com>
2022-04-28 15:48:57 +0200

    Add files via upload

---
+++ matematyka/skrypt.js
@@ -0,0 +1,10 @@
+function obejrz(){
+    let a=parseInt(document.getElementById("jeden").value)
+    let b=parseInt(document.getElementById("dwa").value)
+    if(a==b){
+        document.getElementById("")
+        }
+    else{
+
+        }
+    }
